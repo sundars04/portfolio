ef8698d0f48362979f67d14b59528ee06d2fef11
Sundars0 <Sundars04@outlook.com>
2016-12-08 10:58:50 +0530

    add CRUD functionality for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  before_action :find_post, :only => [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all    
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Your Post successfully saved!"
+      redirect_to @post
+    else
+      render 'new'      
+    end
+  end
+
+  def edit    
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post updated successfully!"
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash.now[:danger] = "Post was deleted!"
+    redirect_to root_path
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
+end
